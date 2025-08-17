148bac5a6b42b2451ac3b6cd9745cd8c5a3ce276
Umar Aftab <aftab.umar@yahoo.com>
2025-08-16 20:35:58 -0600

    Added and fixed the issues with punctuations1

--- components/Stats.tsx
+++ components/Stats.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { motion } from "framer-motion";
-import { TrendingUp, Users, Clock, Award } from "lucide-react";
+import { TrendingUp, Users, Award } from "lucide-react";
 
 const stats = [
   {
